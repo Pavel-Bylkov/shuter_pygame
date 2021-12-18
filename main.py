7215f1d80e8b5a4779d13875b31a476fdca58e46
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-12-18 22:18:30 +0300

    add enemy

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame as pg
+import random
 
 # нам нужны такие картинки:
 img_back = "galaxy.jpg"  # фон игры
 img_hero = "rocket.png"  # герой
+img_enemy = "ufo.png"  # враг
+win_width, win_height = 1200, 800
 
 
 class Hero:
@@ -25,11 +28,28 @@ class Hero:
     def reset(self, win):
         win.blit(self.image, (self.x, self.y))
 
+
+class Enemy:
+    def __init__(self, x, y, speed):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.image = pg.transform.scale(pg.image.load(img_enemy), (70, 50))
+
+    def update(self):
+        self.y += self.speed
+        self.x += random.randint(-self.speed, self.speed)
+        if self.y > win_height:
+            self.y = -50
+            self.x = random.randint(30, win_height - 30)
+
+    def reset(self, win):
+        win.blit(self.image, (self.x, self.y))
+
 # запускаем инициализацию pygame - настройка на наше железо
 pg.init()
 
 # Создаем окошко
-win_width, win_height = 1200, 800
 pg.display.set_caption("Shooter")  # Title у окна
 window = pg.display.set_mode((win_width, win_height))
 back_img = pg.image.load(img_back)  # конвертация любого формата изображений в формат pygame
@@ -38,6 +58,7 @@ background = pg.transform.scale(back_img, (win_width, win_height))
 # создаем спрайты
 hero = Hero()
 
+monster = Enemy(x=win_width//2, y=0, speed=5)
 
 # переменная "игра закончилась": как только там True,
 # в основном цикле перестают работать спрайты
@@ -68,8 +89,11 @@ while run:
         hero.update()
         hero.reset(window)
 
+        monster.update()
+        monster.reset(window)
+
         pg.display.update()
 
     # цикл срабатывает каждую 0.05 секунд
     #time.delay(50)
-    clock.tick(60)
+    clock.tick(40)
