41b18a0fc47e563d8c0a3d5529f6f6a949f3d45a
Pavel-Bylkov <oldman.bpv@gmail.com>
2022-02-26 19:34:07 +0300

    add chamge weapon

--- main.py
+++ main.py
@@ -5,20 +5,33 @@ import time
 # todo Добавить режимы игр
 #ToDo make it so that when rocket and ufo interact the ufo
 # disappears and u get point not u lose game
+# todo add different gun by power
 
-FPS = 35
+FPS = 20
 # запускаем инициализацию pygame - настройка на наше железо
 pg.init()
 pg.font.init()
 
+win_width, win_height = 1200, 800
 # нам нужны такие картинки:
 img_back = "galaxy.jpg"  # фон игры
-img_hero = "rocket.png"  # герой
-img_enemy = "ufo.png"  # враг
-img_over = "gameover.jpeg"
-img_bull = "fireball.png"
+# конвертация любого формата изображений в формат pygame
+back_img = pg.image.load(img_back)
+background = pg.transform.scale(back_img, (win_width, win_height))
+# герой
+img_hero = pg.transform.scale(pg.image.load("rocket.png"), (60, 80))
+# враг
+img_enemy = pg.transform.scale(pg.image.load("ufo.png"), (70, 50))
+# подготавливаем картинку для геймовера
+gameover = pg.transform.scale(pg.image.load("gameover.jpeg"), (win_width, win_height))
+img_bull = pg.transform.scale(pg.image.load("fireball.png"), (15, 15))
+img_bull2 = pg.transform.scale(pg.image.load("blaster.png"), (15, 15))
 img_bum = "Взрыв4.png"
-win_width, win_height = 1200, 800
+images_for_bum = []
+for i in range(1, 11):
+    images_for_bum.append(
+        pg.transform.scale(pg.image.load(img_bum), (i * 10, i * 10))
+    )
 
 # цвета
 WHITE_COLOR = (255, 255, 255)
@@ -27,13 +40,6 @@ RED_COLOR = (255, 0, 0)
 GREEN_COLOR = (0, 255, 0)
 BLUE_COLOR = (0, 0, 255)
 YELLOW_COLOR = (255, 255, 0)
-
-
-images_for_bum = []
-for i in range(1, 11):
-    images_for_bum.append(
-        pg.transform.scale(pg.image.load(img_bum), (i * 10, i * 10))
-    )
 
 
 class Text:
@@ -59,20 +65,37 @@ class Text:
     def reset(self, win):
         win.blit(self.image, (self.x, self.y))
 
-
-class Hero(pg.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.speed = 12
-        self.image = pg.transform.scale(pg.image.load(img_hero), (60, 80))
+class Base(pg.sprite.Sprite):
+    def __init__(self, x, y, speed, img, *args, **kwargs):
+        super().__init__(*args)
+        self.speed = speed
+        self.image = img
         self.rect = self.image.get_rect()
-        self.rect.x = 500
-        self.rect.y = 700
+        self.rect.x = x
+        self.rect.y = y
+
+    def reset(self, win):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+class Hero(Base):
+    def __init__(self, x, y, speed):
+        super().__init__(x=x, y=y, speed=speed, img=img_hero)
         self.bullets = pg.sprite.Group()
         self.reload = time.time()
+        self.health = 3
+        self.health_display = Text(x=20, y=90, text="Health: 3", font_size=30)
+        self.cur_weapon = 1
+        # 1 - blaster, 2 - fireball
+
+    def change_weapon(self):
+        if self.cur_weapon == 1:
+            self.cur_weapon = 2
+        else:
+            self.cur_weapon = 1
 
     def update(self):
         self.bullets.update()
+        self.health_display.update(f"Health: {self.health}")
         keys = pg.key.get_pressed()
         if keys[pg.K_w]:
             self.rect.y -= self.speed
@@ -82,64 +105,82 @@ class Hero(pg.sprite.Sprite):
             self.rect.x -= self.speed
         if keys[pg.K_d]:
             self.rect.x += self.speed
-        if keys[pg.K_SPACE] and time.time() - self.reload > 0.25:
-            self.bullets.add(
-                Bullet(x=self.rect.centerx, y=self.rect.top, speed=10)
-            )
-            self.reload = time.time()
+        if keys[pg.K_t]:
+            self.change_weapon()
+        if self.cur_weapon == 1:
+            if keys[pg.K_SPACE] and time.time() - self.reload > 0.25:
+                self.bullets.add(
+                    Bullet(x=self.rect.centerx, y=self.rect.top, speed=10, power=1)
+                )
+                self.reload = time.time()
+        else:
+            if keys[pg.K_SPACE] and time.time() - self.reload > 0.5:
+                self.bullets.add(
+                    Bullet(x=self.rect.centerx, y=self.rect.top, speed=15, power=3)
+                )
+                self.reload = time.time()
+
+    def get_hit(self):
+        self.health -= 1
+        if self.health == 0:
+            game_over()
 
     def reset(self, win):
-        win.blit(self.image, (self.rect.x, self.rect.y))
+        super().reset(win)
         self.bullets.draw(win)
+        self.health_display.reset(win)
 
-
-class Enemy(pg.sprite.Sprite):
+class Enemy(Base):
     def __init__(self, x, y, speed):
-        super().__init__()
-        self.speed = speed
-        self.image = pg.transform.scale(pg.image.load(img_enemy), (70, 50))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        super().__init__(x=x, y=y, speed=speed, img=img_enemy)
+        self.health = 3
+        self.health_display = Text(x=self.rect.x, y=(self.rect.y-15),
+                                   text="3/3", font_size=20)
 
-    def update(self, player, bums):
+    def update(self, player, bums, win):
         global lives, score
         self.rect.y += self.speed
         self.rect.x += random.randint(-self.speed, self.speed)
+        self.health_display.change_pos(self.rect.x, self.rect.y - 15)
         if self.rect.y > win_height:
             self.rect.y = -50
             self.rect.x = random.randint(30, win_height - 30)
             if lives > 0:
                 lives -= 1
-        if pg.sprite.spritecollide(self, player.bullets, True):
+        if pg.sprite.collide_rect(self, player):
+            player.get_hit()
             bums.add(
                 Bum(self.rect.centerx, self.rect.centery)
             )
             self.rect.y = -50
             self.rect.x = random.randint(30, win_height - 30)
             score += 1
+        coll = pg.sprite.spritecollide(self, player.bullets, True)
+        if coll:
+            for bull in coll:
+                self.health -= bull.power
+        if self.health == 0:
+            bums.add(
+                Bum(self.rect.centerx, self.rect.centery)
+            )
+            self.rect.y = -50
+            self.rect.x = random.randint(30, win_height - 30)
+            score += 1
+            self.health = 3
+        self.health_display.update(f"{self.health}/3")
+        self.health_display.reset(win)
 
-    def reset(self, win):
-        win.blit(self.image, (self.rect.x, self.rect.y))
 
 
-class Bullet(pg.sprite.Sprite):
-    def __init__(self, x, y, speed):
-        super().__init__()
-        self.speed = speed
-        self.image = pg.transform.scale(pg.image.load(img_bull), (10, 5))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+class Bullet(Base):
+    def __init__(self, x, y, speed, power):
+        super().__init__(x=x, y=y, speed=speed, img=img_bull)
+        self.power = power
 
     def update(self):
         self.rect.y -= self.speed
         if self.rect.y < 0:
             self.kill()
-
-    def reset(self, win):
-        win.blit(self.image, (self.rect.x, self.rect.y))
-
 
 class Bum(pg.sprite.Sprite):
     def __init__(self, x, y):
@@ -165,17 +206,18 @@ class Bum(pg.sprite.Sprite):
     def reset(self, win):
         win.blit(self.image, (self.rect.x, self.rect.y))
 
+def game_over():
+    global finish
+
+    finish = True
+    window.blit(gameover, (0, 0))
+
 # Создаем окошко
 pg.display.set_caption("Shooter")  # Title у окна
 window = pg.display.set_mode((win_width, win_height))
-back_img = pg.image.load(img_back)  # конвертация любого формата изображений в формат pygame
-background = pg.transform.scale(back_img, (win_width, win_height))
-
-# подготавливаем картинку для геймовера
-gameover = pg.transform.scale(pg.image.load(img_over), (win_width, win_height))
 
 # создаем спрайты
-hero = Hero()
+hero = Hero(x=500, y=700, speed=12)
 
 # создаем надписи на экране
 score_display = Text(x=20, y=30, text="Score: 0", font_size=30)
@@ -188,7 +230,7 @@ lives = 3
 monsters = pg.sprite.Group()
 for k in range(5):
     monsters.add(
-        Enemy(x=random.randint(3, win_width // 10 - 3) * 10,
+        Enemy(x=random.randint(3, win_width // 10 - 4) * 10,
               y=random.randint(-500, -30),
               speed=random.randint(3, 7))
     )
@@ -215,7 +257,7 @@ while run:
         hero.update()
         hero.reset(window)
 
-        monsters.update(hero, bums)
+        monsters.update(hero, bums, window)
         monsters.draw(window)
 
         bums.update()
@@ -233,8 +275,7 @@ while run:
         lives_display.reset(window)
 
         if lives == 0:
-            window.blit(gameover, (0, 0))
-            finish = True
+            game_over()
 
         pg.display.update()
 
