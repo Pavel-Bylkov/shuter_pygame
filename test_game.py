62a1c02328a2a913500b47a8cf0a98c5931869d6
unknown <nsk-adm@nsk1-rez-01.licey22.local>
2022-09-17 21:47:37 +0700

    add test_game

---
+++ test_game.py
@@ -0,0 +1,22 @@
+import pygame as pg
+
+pg.init()
+
+win_width, win_height = 1200, 800
+
+window = pg.display.set_mode((win_width, win_height))
+run = True
+clock = pg.time.Clock()
+while run:
+    # событие нажатия на кнопку Закрыть
+    for e in pg.event.get():
+        if e.type == pg.QUIT:
+            run = False
+
+    window.fill((30, 200, 50))
+    test = 565656 * 345
+    pg.display.update()
+
+    # цикл срабатывает каждую 0.05 секунд
+    #time.delay(50)
+    clock.tick(60)
